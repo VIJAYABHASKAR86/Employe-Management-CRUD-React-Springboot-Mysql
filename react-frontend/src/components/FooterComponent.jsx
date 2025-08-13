ebd5e68fcbdce5705ca4110050ba0eb88fee0a8d
VIJAYABHASKAR86 <gvijayb19@gmail.com>
2025-08-13 13:49:52 +0530

    first commit

---
+++ react-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <footer className = "footer">
+                <div><a className="navbar-brand">@ All Rights Reserved 2025 </a></div>                </footer>
+            </div>
+        )
+    }
+}
+
+export default FooterComponent
